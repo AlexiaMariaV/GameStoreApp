6a41bab9bba4d2a881ae96efe9e3e170191d7d77
AlexiaMariaV <alexiavoina2004@gmail.com>
2024-12-12 15:31:33 +0200

    Added test scheme

--- src/Tests/Tests.java
+++ src/Tests/Tests.java
@@ -5,4 +5,25 @@ public class Tests {
 
     @Test
     public void TestCRUDCustomer() {}
+
+    @Test
+    public void TestCRUDAdmin() {}
+
+    @Test
+    public void TestCRUDDeveloper() {}
+
+    @Test
+    public void TestCRUDGame() {}
+
+    @Test
+    public void TestCRUDDiscount() {}
+
+    @Test
+    public void TestCRUDReview() {}
+
+    @Test
+    public void TestCRUDShoppingCart() {}
+
+    @Test
+    public void TestBuyingGame() {}
 }
